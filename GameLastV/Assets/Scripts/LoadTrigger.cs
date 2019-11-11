f1a388873395a6c3bb137729a13fc152163568d8
Yura <krav17yura@gmail.com>
2019-11-11 09:35:28 +0200

    last

---
+++ GameLastV/Assets/Scripts/LoadTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTrigger : MonoBehaviour
+{
+    public int numerOfScene;
+    public string playerTag;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("!");
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("Quit");
+                Application.Quit();
+            }
+        }
+    }
+}
+
+
